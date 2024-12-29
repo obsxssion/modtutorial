107d104c7454b94edb1212b05a18d54695ba3a2f
ste <stefanovolpe006@gmail.com>
2024-12-29 13:08:58 +0100

    Initial commit

---
+++ src/main/java/net/ste/tutorial/TutorialClient.java
@@ -0,0 +1,10 @@
+package net.ste.tutorial;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
